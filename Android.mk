4368b671e591c65b216bbda9ff10c7c3acda749b
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2022-04-21 16:09:06 +0200

    Add hidden API

--- Android.mk
+++ Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := lawnchair-hiddenapi-package-whitelist.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-org.lawnchair.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
@@ -16,6 +24,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-org.lawnchair.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-org.lawnchair.xml,lawnchair-hiddenapi-package-whitelist.xml
 include $(BUILD_PREBUILT)
 
